e14ae62f3fddfd26ed6221e2d599aca7fc19c97e
ludivine <ludivine.raby@gmail.com>
2025-03-21 11:58:18 +0100

    Fill the table performance with json datas

--- fill_performance.py
+++ fill_performance.py
@@ -1,11 +1,12 @@
 import requests
 import json
+from models.models import insert_performance
+import sqlite3
 
-# URL de ton API
-API_URL = "http://localhost:8000/performance/create"  
+
 
 #Token
-with open("data.json", "r") as file:  # Remplace "data.json" par le chemin de ton fichier
+with open("json_file/bonus_sbj.json", "r") as file:  # Remplace "data.json" par le chemin de ton fichier
     json_data = json.load(file)
 
 # Extract the values
@@ -18,14 +19,19 @@ payload = {
     "completion_date": "2025-03-21"  # to adapt
 }
 
-# # En-têtes HTTP
-# headers = {
-#     "Authorization": f"Bearer {TOKEN}",
-#     "Content-Type": "application/json"
-# }
+# # Envoi de la requête POST
+# response = requests.post(API_URL, json=payload, headers=headers)
 
-# Envoi de la requête POST
-response = requests.post(API_URL, json=payload, headers=headers)
+# # Affichage de la réponse
+# print(response.status_code, response.json())
 
-# Affichage de la réponse
-print(response.status_code, response.json())
+
+# Insert users and athlete
+conn = sqlite3.connect('database.db', check_same_thread=False)
+c = conn.cursor()
+insert_performance(athlete_id=8,completion_date="03/01/2025",vo2max=payload["vo2max"],
+                       hr=payload["hr"],cadence=payload["cadence"],ppo=payload['ppo'],rf=payload['rf'])
+
+print("1 performance created!")
+
+conn.close()
